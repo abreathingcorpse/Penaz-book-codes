ff853d267836c58a09494cd04fbca58a2bf68d72
abreathingcorpse <raul.rst11@gmail.com>
2022-10-31 21:55:15 -0600

    Fixed typo in Code Listing 10

--- random_seed.cpp
+++ random_seed.cpp
@@ -9,7 +9,7 @@ int main() {
 	// Small presentation
 	std::cout << "This generator will now generate 10 random numbers" << std::endl;
 	// Output 10 random numbers
-	for (int = i 0; i < 10; ++i {
+	for (int i = 0; i < 10; ++i {
 		std::cout << rand() << std::endl;
 	}
 	// Finish the program
