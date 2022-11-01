1ed881c15984eae50eb45e4df58eae2f3a3133b8
abreathingcorpse <raul.rst11@gmail.com>
2022-10-31 21:52:26 -0600

    Added Code Listing 10

---
+++ random_seed.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main() {
+	// First of all we get the seed
+	unsigned int seed;
+	std::cout << "Type the seed: ";
+	std::cin >> seed;
+	// Now we seed the randomizer
+	srand(seed);
+	// Small presentation
+	std::cout << "This generator will now generate 10 random numbers" << std::endl;
+	// Output 10 random numbers
+	for (int = i 0; i < 10; ++i {
+		std::cout << rand() << std::endl;
+	}
+	// Finish the program
+	return 0;
+}
